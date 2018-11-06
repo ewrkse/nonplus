975a127b6bfef70cd74a27ff79f55735dab538a2
Erik Berglund <erik@lap.ewrk.se>
2018-11-06 23:28:30 +0100

    Initial commit

---
+++ firefox/nonplus.js
@@ -0,0 +1,18 @@
+var css = document.createElement("style");
+css.type = "text/css";
+css.innerHTML = '\
+[href^="\/plus\/"] * { \
+	background-color: #e6e8fc; \
+} \
+[href^="\/plus\/"] h1:before, \
+[href^="\/plus\/"] h2:before, \
+[href^="\/plus\/"] h3:before, \
+[href^="\/plus\/"] h4:before, \
+[href^="\/plus\/"] h5:before, \
+[href^="\/plus\/"] h6:before { \
+	content: "PLUS"; \
+	margin: 0 .25em 0 0; \
+	color: rgba(0, 0, 0, 0); \
+	background: url("' + browser.extension.getURL("plus.svg") + '") no-repeat center; \
+}';
+document.head.appendChild(css);
