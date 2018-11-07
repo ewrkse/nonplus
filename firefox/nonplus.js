35564527ba940bb9876486f1c0209389944bf3f1
Erik Berglund <erik@ewrk.se>
2018-11-07 08:09:46 +0100

    Cleanup of CSS

--- firefox/nonplus.js
+++ firefox/nonplus.js
@@ -1,22 +1,15 @@
 var css = document.createElement("style");
 css.type = "text/css";
 css.innerHTML = '\
-[href^="https:\/\/affarsliv24.vk.se\/plus\/"] *, \
-[href^="\/plus\/"] * { \
+[href*="\/plus\/"] * { \
 	background-color: #e6e8fc; \
 } \
-[href^="https:\/\/affarsliv24.vk.se\/plus\/"] h1:before, \
-[href^="https:\/\/affarsliv24.vk.se\/plus\/"] h2:before, \
-[href^="https:\/\/affarsliv24.vk.se\/plus\/"] h3:before, \
-[href^="https:\/\/affarsliv24.vk.se\/plus\/"] h4:before, \
-[href^="https:\/\/affarsliv24.vk.se\/plus\/"] h5:before, \
-[href^="https:\/\/affarsliv24.vk.se\/plus\/"] h6:before, \
-[href^="\/plus\/"] h1:before, \
-[href^="\/plus\/"] h2:before, \
-[href^="\/plus\/"] h3:before, \
-[href^="\/plus\/"] h4:before, \
-[href^="\/plus\/"] h5:before, \
-[href^="\/plus\/"] h6:before { \
+[href*="\/plus\/"] h1:before, \
+[href*="\/plus\/"] h2:before, \
+[href*="\/plus\/"] h3:before, \
+[href*="\/plus\/"] h4:before, \
+[href*="\/plus\/"] h5:before, \
+[href*="\/plus\/"] h6:before { \
 	content: "PLUS"; \
 	margin: 0 .25em 0 0; \
 	color: rgba(0, 0, 0, 0); \
