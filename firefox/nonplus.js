06cb79aa377d7e3521089958e5c710bb058ef24a
Erik Berglund <erik@lap.ewrk.se>
2018-11-07 08:00:56 +0100

    Updated CSS to cover Affärsliv and updated version number.

--- firefox/nonplus.js
+++ firefox/nonplus.js
@@ -1,9 +1,16 @@
 var css = document.createElement("style");
 css.type = "text/css";
 css.innerHTML = '\
+[href^="https:\/\/affarsliv24.vk.se\/plus\/"] *, \
 [href^="\/plus\/"] * { \
 	background-color: #e6e8fc; \
 } \
+[href^="https:\/\/affarsliv24.vk.se\/plus\/"] h1:before, \
+[href^="https:\/\/affarsliv24.vk.se\/plus\/"] h2:before, \
+[href^="https:\/\/affarsliv24.vk.se\/plus\/"] h3:before, \
+[href^="https:\/\/affarsliv24.vk.se\/plus\/"] h4:before, \
+[href^="https:\/\/affarsliv24.vk.se\/plus\/"] h5:before, \
+[href^="https:\/\/affarsliv24.vk.se\/plus\/"] h6:before, \
 [href^="\/plus\/"] h1:before, \
 [href^="\/plus\/"] h2:before, \
 [href^="\/plus\/"] h3:before, \
